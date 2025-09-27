87170dce2a1db0ce5eb35d1fbe857842c84495d5
Rona <142495887+rona94@users.noreply.github.com>
2025-09-27 11:14:41 +0800

    remove content

--- src/utils/common-utils.ts
+++ src/utils/common-utils.ts
@@ -1,11 +1,1 @@
-export class CommonUtils {
-  private static _navigate: any;
-
-  static init(navigate: any) {
-    this._navigate = navigate;
-  }
-
-  static navigate() {
-    this._navigate(-1);
-  }
-}
+export class CommonUtils {}
