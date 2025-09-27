a22f2cf6bba0d3a19707d868aa19be7a95ca986f
Rona <142495887+rona94@users.noreply.github.com>
2025-09-27 10:21:13 +0800

    fix breadcrumbs

--- src/components/breadcrumbs/breadcrumbs-model.ts
+++ src/components/breadcrumbs/breadcrumbs-model.ts
@@ -7,15 +7,13 @@ export const useBreadcrumbs = () => {
   const { breadcrumbs } = location.state;
 
   const onClick = (path: string = "", index: number = 0) => {
-    breadcrumbs.splice(index + 1);
+    breadcrumbs.splice(index);
 
     navigate(path, {
       state: {
         breadcrumbs: breadcrumbs.pop(),
       },
     });
-    // const path = (breadcrumbs?.length - index - 1) * -1;
-    // navigate(path);
   };
 
   return { breadcrumbs, location, onClick };
